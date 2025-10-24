1b5648b7f407c0b2e2410aed4e5199ed7884d08b
Ahmed Zaki Al-Gabaly <149044701+AhmedWritesCode@users.noreply.github.com>
2025-10-24 23:10:56 +0800

    Implement routing and details view for products
    Added Angular routes for home and product details pages. Updated AppComponent to display product links and conditional info. Enhanced DetailsComponent to accept product id via input and render product details. Enabled component input binding in app config.

--- projects/08-routing-recap/src/app/app.component.ts
+++ projects/08-routing-recap/src/app/app.component.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: ` <h1>Welcome to {{ title }}!</h1> `,
+  imports: [RouterOutlet, RouterLink],
+  template: ` <h1>Welcome to {{ title }}!</h1>
+              <router-outlet />
+  @for(title of productTitles; track title){
+    <a [routerLink] = "['details', $index]">{{title}}</a> <!-- $index is a built-in variable that gives the index of the current item in the loop -->
+    @if (Number(title[8]) == 1){
+    <p>Carrots</p>
+    }
+      @else if (Number(title[8]) == 2) {
+      <p>Broccoli</p>
+      }
+      @else {
+      <p>Spinach</p>
+    }
+    <p> - - </p>
+  }
+  `,
 })
 export class AppComponent {
   title = '08-routing-recap';
 
   productTitles = ['Product 1', 'Product 2', 'Product 3'];
+  protected readonly Number = Number;
 }
