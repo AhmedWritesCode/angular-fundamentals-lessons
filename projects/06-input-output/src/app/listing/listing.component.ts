82b0df6ab3ca9227dab95d1af22218e4ff0353a1
Ahmed Zaki Al-Gabaly <149044701+AhmedWritesCode@users.noreply.github.com>
2025-10-24 23:10:26 +0800

    Implement car listing input/output and basic routing
    Refactored car listing to use ListingComponent with @Input and @Output for car data and save events. Added routing to the 07-routing-basics project, including a default route to GreetingsComponent and configuration of RouterModule and provideRouter.

--- projects/06-input-output/src/app/listing/listing.component.ts
+++ projects/06-input-output/src/app/listing/listing.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Car } from '../car';
 
@@ -6,7 +6,43 @@ import { Car } from '../car';
   selector: 'app-listing',
   standalone: true,
   imports: [CommonModule],
-  template: ` <!-- listing markup goes here --> `,
+  template: `       <!-- This article element represents and entire listing -->
+  <article class="listing">
+    <div class="image-parent">
+      <img class="product-image" src="https://placehold.co/100x100" />
+    </div>
+    <section class="details">
+      <p class="title">{{ car.make + ' ' + car.model }}</p>
+      <hr />
+      <p class="detail">
+        <span>Year</span>
+        <span>{{ car.year }}</span>
+      </p>
+      <div class="detail">
+        <span>Transmission</span>
+        <span>{{ car.transmission }}</span>
+      </div>
+      <p class="detail">
+        <span>Mileage</span>
+        <span>{{ car.miles }}</span>
+      </p>
+      <p class="detail">
+        <span>Price</span>
+        <span>{{ car.price }}</span>
+      </p>
+    </section>
+  </article>
+  <button (click) = "handleCarSave()"> Save </button>
+  <!-- end car listing markup -->
+  `,
   styles: ``,
 })
-export class ListingComponent {}
+export class ListingComponent {
+  @Input({required: true}) car!: Car; // ! indicates that this property will be definitely assigned
+
+  @Output() carSaved = new EventEmitter<Car>(); // carSaved is an EventEmitter, this is supposed to emit
+
+  handleCarSave() {
+    this.carSaved.emit(this.car);
+  }
+}
